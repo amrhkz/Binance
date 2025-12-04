c336722794eba7ebacb409e3dd45546e9dae8dec
AmirHosein <karimzade.ah@gmail.com>
2025-12-04 17:14:15 +0330

    backend created

---
+++ back/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONT_PORT,
+  })
+);
+
+const mongoUri = process.env.MONGO_LOCAL;
+
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    const dbType = mongoUri === process.env.MONGO_LOCAL ? "LOCAL" : "ATLAS";
+    console.log(`MongoDB Connected (${dbType})`);
+  })
+  .catch((err) => console.error("MongoDB Error:", err));
+
+app.listen(process.env.BACK_PORT, () =>
+  console.log(`Server running on http://localhost:${process.env.BACK_PORT}`)
+);
